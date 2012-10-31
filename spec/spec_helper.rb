0fbc8abe387b2ca57879951d04b8f485a7572bbc
Alexander Wenzowski <alexander@wenzowski.com>
2012-10-31 05:09:06 -0400

    dry specs using shared examples

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -31,6 +31,7 @@ RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true
   config.filter_run :focus
+  config.alias_it_should_behave_like_to :it_receives, 'receives:'
 
   # Run specs in random order to surface order dependencies. If you find an
   # order dependency and want to debug it, you can fix the order by providing
