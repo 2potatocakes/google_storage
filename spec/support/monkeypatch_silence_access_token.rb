58a0a4f2e2d546f8462f4a6899678a11f05ae8ba
Alexander Wenzowski <alexander@wenzowski.com>
2012-10-30 20:42:26 -0400

    make way for the monkeypatch (future-proofing)

--- spec/support/monkeypatch_silence_access_token.rb
+++ spec/support/monkeypatch_silence_access_token.rb
@@ -3,16 +3,12 @@ require 'vcr'
 
 module GoogleStorage
   class Client
-    def refresh_access_token(token, options={})
-      options['grant_type'] = 'refresh_token'
-      response = post_request('accounts.google.com', '/o/oauth2/token', token,
-                              options)
-      VCR.configure do |c| 
-        c.filter_sensitive_data('____SILENCED_access_token____') do 
+    def after_refresh_access_token(response)
+      VCR.configure do |c|
+        c.filter_sensitive_data('____SILENCED_access_token____') do
           response['access_token']
         end
       end
-      response
     end
   end
 end
